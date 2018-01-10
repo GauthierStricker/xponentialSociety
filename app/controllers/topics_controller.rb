c40dfe3e7a015f7b086846fb01678d854131c14a
Gauthier Stricker <stricker.gauthier@gmail.com>
2018-01-10 14:13:33 +0100

    Injected Ruby In categories

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,11 +5,12 @@ class TopicsController < ApplicationController
 
   def show          # GET /topics/:id
     @topic = Topic.find(params[:id])
+    @videos = @topic.videos
   end
 
   private
 
   def topic_params
-    params.require(:topic).permit(:name)
+    params.require(:topic).permit(:name, :id)
   end
 end
