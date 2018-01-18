eb42fea9bad45943402768b050a9bfc601abf6b0
Gauthier Stricker <stricker.gauthier@gmail.com>
2018-01-18 13:36:26 +0100

    Finish Video Show static

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110222658) do
+ActiveRecord::Schema.define(version: 20180113134701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20180110222658) do
     t.datetime "updated_at", null: false
     t.index ["topic_id"], name: "index_topics_videos_on_topic_id"
     t.index ["video_id"], name: "index_topics_videos_on_video_id"
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "name"
+    t.string "total_duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
